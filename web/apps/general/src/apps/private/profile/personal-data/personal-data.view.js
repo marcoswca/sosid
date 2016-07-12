c1fb95257f716653720726906fa4d2a4070e608a
Paulo Regis <pauloregis@outlook.com>
2016-07-12 12:16:17 -0300

    adicionar topo do master, menu do profile e conteúdo do personal data

--- web/apps/general/src/apps/private/profile/personal-data/personal-data.view.js
+++ web/apps/general/src/apps/private/profile/personal-data/personal-data.view.js
@@ -15,11 +15,24 @@
         // Public variables
         self.viewName = 'Personal Data View';
 
+        self.user = {
+            firstName: 'Paulo Régis',
+            lastName: 'de Medeiros Cavalcante',
+            age: '26',
+            sex: 'male',
+            bloodType: 'Group AB',
+            address: 'Düesbergweg 10, 48153 Münster',
+            plan: 'Lorem Ipsum sit dolor amet plan',
+            period: 'Good from 25 July, 2016 to 31 March, 2017'
+        };
+
+        self.userFullname = self.user.firstName + " " + self.user.lastName;
+
         // Public methods
 
         // Private methods
         return (function init() {
-
+            console.log(self.user);
         })();
     }
 })();
