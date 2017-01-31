c2e624b1f8afceb5d8a92b49a31dcbcbf09fa8c8
Marcos Arruda <marcos.arruda@splab.ufcg.edu.br>
2017-01-31 11:10:12 -0300

    Fixes

--- web/apps/general/src/apps/private/components/family-doctor/family-doctor.js
+++ web/apps/general/src/apps/private/components/family-doctor/family-doctor.js
@@ -24,12 +24,33 @@
             // Private variables
             var self = this;
             self.user = {};
+            self.disabledFields = true;
 
             DoctorApi.getFamilyDoctors().then(function successCallback(data) {
                 self.user.familyDoctors = data.rows;
             }, function errorCallback(reason) {
 
             });
+
+            self.enableFields = function() {
+                self.disabledFields = false;
+            };
+
+            self.save = function(fmDoctor) {
+                var copy = angular.copy(fmDoctor);
+                delete copy.id;
+                DoctorApi.update(fmDoctor.id, copy).then(function successCallback(data) {
+                    self.disabledFields = true;
+                    // var keys = Object.keys(doctor);
+                    // keys.forEach(function(key, index) {
+                    //     fmDoctor[key] = doctor[key];
+                    //     doctor[key] = undefined;
+                    // });
+                    console.log(data);
+                }, function errorCallback(reason) {
+                    console.log(reason);
+                });
+            };
 
             return (function init() {})();
 
