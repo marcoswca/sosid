1cc8c73dadf98bb939f540922283d8ac4765d6b5
Marcos Arruda <marcoswca1@gmail.com>
2017-02-13 16:12:52 -0300

    bug fix relationship

--- web/apps/general/src/_models/emergency-contact.model.js
+++ web/apps/general/src/_models/emergency-contact.model.js
@@ -37,8 +37,6 @@
                     }
                 },
                 relationship: {
-                    type: '',
-                    values: ['Single parent','Spouse','Husband','Wife','Parent','Father','Mother','Child','Son','Daughter','Sibling','Brother','Sister','Extended family','Grandparent','Grandfather','Grandmother','Grandson','Granddaughter','Uncle','Aunt','Cousin','Nephew','Niece','Boyfriend','Girlfriend','Friend','know','Working colleague'],
                     label: true,
                     validate: {
                         required: {
