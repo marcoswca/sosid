b14268f44c0dba618ef4ae4467fa394014079c92
Marcos Arruda <marcoswca1@gmail.com>
2017-04-06 20:54:53 -0300

    Fix build

---
+++ web/libs/core/config.js
@@ -0,0 +1,34 @@
+(function () {
+
+    'use strict';
+
+    var dependencies = [
+        'pascalprecht.translate',
+        'ngSanitize'
+    ];
+
+    angular
+        .module('core.config', dependencies)
+        .constant('APP_CONFIG', {
+            URL: {
+                // APP_URL: 'http://35.156.40.94/sosid/',
+                APP_URL: 'https://sosid.pimentagroup.de/app',
+                API_URL: 'https://api.sosid.pimentagroup.de'
+                // API_URL: 'http://35.156.40.94'
+            }
+        })
+        .config(config);
+
+    /** @ngInject */
+    function config($translateProvider) {
+        $translateProvider.useSanitizeValueStrategy(null);
+        $translateProvider
+            .useStaticFilesLoader({
+                prefix: './assets/locales/',
+                suffix: '.json?v=' + APP_VERSION
+            })
+            .fallbackLanguage('en')
+            .preferredLanguage('de');
+    }
+
+})();
