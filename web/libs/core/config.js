d5b5867a7555f94371a284a86a24637bcdf8c987
Marcos Arruda <marcoswca1@gmail.com>
2017-04-13 12:18:30 -0300

    Fix endpoint

--- web/libs/core/config.js
+++ web/libs/core/config.js
@@ -11,12 +11,8 @@
         .module('core.config', dependencies)
         .constant('APP_CONFIG', {
             URL: {
-                // APP_URL: 'http://35.156.40.94/sosid/',
                 APP_URL: 'https://sosid.pimentagroup.de/app',
-                // APP_URL: 'http://localhost:3001',
-                // API_URL: 'http://localhost:3000'
                 API_URL: 'https://api.sosid.pimentagroup.de'
-                // API_URL: 'http://35.156.40.94'
             }
         })
         .config(config);
